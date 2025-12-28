5adc579e1bda0f9c3c245dd486ef5acabb4d32f0
Muhammad Talha <muhammadtalha.quant@gmail.com>
2025-12-28 16:47:11 +0500

    added missing header guard

--- src/cli/CLInterface.hpp
+++ src/cli/CLInterface.hpp
@@ -1,23 +1,20 @@
+#ifndef MMQLI_CLI_HPP
+#define MMQLI_CLI_HPP
+
 namespace mmqli::cli {
     class CLInterface {
         int argument_count;
         char **argument_vector;
-
         static void unknown_flag_error() noexcept;
-
         static void print_copyright_notice() noexcept;
-
         static void show_help_message();
-
         static void clear_screen_platform_agnostic();
-
         static void start_interpreter();
-
         static void show_version_plus_copyright();
-
     public:
         CLInterface(int argsC, char **argvA);
-
         void start_cli();
     };
 } // mmqli::cli
+
+#endif // MMQLI_CLI_HPP
