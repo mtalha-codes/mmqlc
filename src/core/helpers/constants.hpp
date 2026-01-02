3418a9fa16df2722fa97efcc892a72cc9fccf419
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-01-02 18:12:06 +0500

    added the two removed constants real infinity and real NaN

--- src/core/helpers/constants.hpp
+++ src/core/helpers/constants.hpp
@@ -4,10 +4,12 @@
 #include "boost_fwd.hpp"
 
 namespace mmqli::core::constants {
-    const float1000 arbitrary_precise_pi(3.141592653589793238462643383279);
-    const float1000 real_one(1);
-    const float1000 real_zero(0);
-    const complex1000 complex_one(real_one, real_zero);
-    const complex1000 complex_zero(real_zero, real_zero);
+    const float1000 arbitrary_precise_pi{3.141592653589793238462643383279};
+    const float1000 real_one{1};
+    const float1000 real_zero{0};
+    const float1000 real_infinity{std::numeric_limits<float1000>::infinity()};
+    const float1000 real_nan{std::numeric_limits<float1000>::quiet_NaN()};
+    const complex1000 complex_one{real_one, real_zero};
+    const complex1000 complex_zero{real_zero, real_zero};
 }
 #endif
